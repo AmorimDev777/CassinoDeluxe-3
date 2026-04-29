87be27d4665bed29ea4a2cc7e95152cc404448c0
Amorim_gk.12 <gabriel.amorim.etec@gmail.com>
2026-04-29 12:25:28 +0000

    Campeonatos detalhes

--- src/components/CardEsportes.jsx
+++ src/components/CardEsportes.jsx
@@ -1,26 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 
-function CardEsportes({confronto, onClick}) {
-    const [times, setTimes] = useState([])
-    const [campeonatos, setCampeonatos] = useState([])
-    useEffect(() => {
-        const getDados = async () => {
-            axios.get("/db/db.json")
-            .then((response) => {
-                setTimes(response.data.times)
-                setCampeonatos(response.data.campeonatos)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-        }
-        getDados()
-    }, []);
-    const campeonato = campeonatos.find(campeonato => campeonato.id === Number(confronto.campeonato))
-    const time1 = times.find(time => time.nome === confronto.time1 && time.esporte === campeonato.esporte)
-    const time2 = times.find(time => time.nome === confronto.time2 && time.esporte === campeonato.esporte)
-
+function CardEsportes({confronto, time1, time2, campeonato, onClick}) {
     if (!time1 || !time2) {
         return <div className="flex justify-center items-center fixed top-0 left-0 h-screen w-full text-9xl text-amber-400 backdrop-blur-lg"><i class="fa-solid fa-spinner animate-[spin_0.5s_linear_infinite]"></i></div>
     }
@@ -33,28 +13,28 @@ function CardEsportes({confronto, onClick}) {
         >       
             <div className="flex h-full">
                 <div className="flex justify-center items-center flex-col h-full text-xs text-zinc-500 aspect-square">
-                    <p>{confronto.data}</p>
-                    <p>{confronto.hora}</p>
+                    <p>{confronto?.data}</p>
+                    <p>{confronto?.hora}</p>
                 </div>
                 <div className="flex justify-center items-center flex-col h-full py-3 gap-1">
                     <span className="flex h-[50%] w-full gap-1">
-                        <img src={time1.escudo} alt="" className="h-full aspect-square object-contain"/>
-                        <h1>{time1.nome}</h1>
+                        <img src={time1?.escudo} alt="" className="h-full aspect-square object-contain"/>
+                        <h1>{time1?.nome}</h1>
                     </span>
                     <span className="flex h-[50%] w-full gap-1">
-                        <img src={time2.escudo} alt="" className="h-full aspect-square object-contain"/>
-                        <h1>{time2.nome}</h1>
+                        <img src={time2?.escudo} alt="" className="h-full aspect-square object-contain"/>
+                        <h1>{time2?.nome}</h1>
                     </span>
                 </div>
             </div>
 
             <div className="flex justify-center items-center flex-col h-full py-3 pr-5">
                 <img 
-                    src={campeonato.logo} 
+                    src={campeonato?.logo} 
                     alt="" 
                     className="h-[70%] object-contain" 
                 />
-                <h1 className="text-sm">{campeonato.nome}</h1>
+                <h1 className="text-sm">{campeonato?.nome}</h1>
             </div>
         </div>
     )
--- src/pages/Esportes.jsx
+++ src/pages/Esportes.jsx
@@ -10,12 +10,14 @@ function Esportes() {
     const [saldo, setSaldo] = useState(localStorage.getItem('saldo') || 0)
     const [confrontos, setConfrontos] = useState([])
     const [times, setTimes] = useState([])
+    const [campeonatos, setCampeonatos] = useState([])
     useEffect(() => {
         const getDados = async () => {
             axios.get("/db/db.json")
             .then((response) => {
                 setConfrontos(response.data.confrontos)
                 setTimes(response.data.times)
+                setCampeonatos(response.data.campeonatos)
             })
             .catch((error) => {
                 console.error(error);
@@ -26,14 +28,26 @@ function Esportes() {
     return (
         <main className="flex flex-col h-screen w-screen p-10 pt-[16vh] gap-5 bg-black">
             <Navbar saldo={Number(saldo)}/>
-            {confrontos.map((confronto, index) => (
-                <CardEsportes 
+            {confrontos.map((confronto, index) => {
+                const time1 = times.find(t => (
+                    t.nome === confronto.time1
+                ))
+                const time2 = times.find(t => (
+                    t.nome === confronto.time2
+                ))
+                const campeonato = campeonatos.find(c => (
+                    c.id === confronto.campeonato
+                ))
+                return <CardEsportes 
                 key={index}
                 confronto={confronto}
+                time1={time1}
+                time2={time2}
+                campeonato={campeonato}
                 onClick={() => {
                     navigate(`/jogo?confronto=${confronto.id}&time1=${confronto.time1}&time2=${confronto.time2}&esporte=${confronto.esporte}&campeonato=${confronto.campeonato}`)
                 }}/>
-            ))}
+            })}
         </main>
     )
 }
